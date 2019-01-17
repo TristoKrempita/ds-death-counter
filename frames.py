1c41540122b0f3f01c58aa8ad14c25461f607b14
unknown <tristan.vujovic@gmail.com>
2019-01-17 15:29:09 +0100

    Minor textual and variable name changes

--- frames.py
+++ frames.py
@@ -5,19 +5,22 @@ import os
 
 now = datetime.datetime.now()
 
-death_frame_count = 0
+death_count = 0
 frame_count = 0
 skip_frames = 0
 threshold = .2
 
+#create frames/
 if not os.path.exists("frames"):
     os.makedirs("frames")
 
+#CLI
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video", required=True,
 	help="path to our file")
 args = vars(ap.parse_args())
 
+#Capture video and get first frame
 vidcap = cv2.VideoCapture(args["video"])
 success,image = vidcap.read()
 
@@ -25,8 +28,6 @@ is_found = False
 was_found = False
 
 template = cv2.imread('youdied.png')
-w, h = template.shape[:-1]
-frame_name = f"frames/frame.jpg"
 while success:
 
     skip_frames += 1
@@ -43,21 +44,22 @@ while success:
 
     #finding "you died" text
     res = cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED)
-    _, max_val, _, _ = cv2.minMaxLoc(res)
-    print("Max Val: ", max_val)
+    max_val= cv2.minMaxLoc(res)[1]
+    
 
     is_found = max_val >= threshold
+    print(f"Found death: {is_found}")
     if is_found:
         prev_frame = image
 
     #shows image of death with frame number as name
     if was_found and not(is_found):
-        death_frame_count += 1
+        death_count += 1
         cv2.imwrite(f"frames/frame{frame_count}.jpg", prev_frame)
 
     was_found = is_found
 
 print('='*50)
 secs = (datetime.datetime.now()-now).seconds
-print("Elapsed time: %sh:%sm:%ss" %((secs//3600)%24, (secs//60)%60, secs%60))
-print("Deaths registered: ", death_frame_count)
+print(f"Elapsed time: {(secs//3600)%24}h:{(secs//60)%60}m:{secs%60}s")
+print(f"Deaths registered: {death_count}")
